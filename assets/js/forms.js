4c006767ad85c506459dc7bc62f2246a48e7d55d
Oleksandra <sashkapilipon@gmail.com>
2023-07-21 11:46:14 +0300

    Added filtration section to new branch

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -152,12 +152,13 @@ class ModalDoctor {
     this.form.addOption("option", "Звичайна");
     this.form.addOption("option", "Пріоритетна");
     this.form.addOption("option", "Невідкладна");
-    this.form.addInput("ПІБ", "text", "namePatient", "input-login");
 
     this.form.addSelect("changeStatus", "select-status");
     this.form.addOption("option-status", "-- none --", "Статус");
     this.form.addOption("option-status", "Відкритий", "Відкритий");
     this.form.addOption("option-status-close", "Закритий", "Закритий");
+
+    this.form.addInput("ПІБ", "text", "namePatient", "input-login");
   }
 }
 
