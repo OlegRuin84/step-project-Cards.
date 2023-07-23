fcf48c1546231d0fa4c8755d5188c79b81fed0e4
Oleksandra <sashkapilipon@gmail.com>
2023-07-23 15:23:40 +0300

    added drag_drop(worked)

---
+++ assets/js/drag_drop.js
@@ -0,0 +1,40 @@
+class DragAndDrop {
+  constructor(contrainer) {
+    this.container = contrainer;
+  }
+
+  init() {
+    this.container.addEventListener(`dragstart`, (evt) => {
+      evt.target.classList.add(`selected`);
+    });
+
+    this.container.addEventListener(`dragend`, (evt) => {
+      evt.target.classList.remove(`selected`);
+    });
+
+    this.container.addEventListener(`dragover`, (evt) => {
+      evt.preventDefault();
+
+      const activeElement = this.container.querySelector(`.selected`);
+
+      const currentElement = evt.target;
+
+      const isMoveable =
+        activeElement !== currentElement &&
+        currentElement.classList.contains(`card`);
+
+      if (!isMoveable) {
+        return;
+      }
+
+      const nextElement =
+        currentElement === activeElement.nextElementSibling
+          ? currentElement.nextElementSibling
+          : currentElement;
+
+      this.container.insertBefore(activeElement, nextElement);
+    });
+  }
+}
+
+export default DragAndDrop;
